86c558443aa1db58b032c2f55483cb77c3ebc8aa
“Chernohodec” <leva.shirokov@gmail.com>
2024-04-04 19:26:05 +0300

    update

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,5 +1,15 @@
 const whale1 = document.getElementsByClassName('whales-wrapper__whale1');
 const whale2 = document.getElementsByClassName('whales-wrapper__whale2');
+const whale3 = document.getElementsByClassName('whales-wrapper__whale3');
+const whale4 = document.getElementsByClassName('whales-wrapper__whale4');
+const whale5 = document.getElementsByClassName('whales-wrapper__whale5');
+const whale6 = document.getElementsByClassName('whales-wrapper__whale6');
+const cloud1 = document.getElementsByClassName('whales-wrapper__cloud1');
+const cloud2 = document.getElementsByClassName('whales-wrapper__cloud2');
+const cloud3 = document.getElementsByClassName('whales-wrapper__cloud3');
+const cloud4 = document.getElementsByClassName('whales-wrapper__cloud4');
+const cloud5 = document.getElementsByClassName('whales-wrapper__cloud5');
+const cloud6 = document.getElementsByClassName('whales-wrapper__cloud6');
 const rock1 = document.getElementsByClassName('whales-wrapper__rock1');
 const rock2 = document.getElementsByClassName('whales-wrapper__rock2');
 
@@ -15,6 +25,38 @@ new simpleParallax(whale2, {
     delay: 0,
     orientation: 'up',
     scale: 2,
+    overflow: true,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
+
+new simpleParallax(whale3, {
+    delay: 0,
+    orientation: 'up',
+    scale: 1.5,
+    overflow: true,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
+
+new simpleParallax(whale4, {
+    delay: 0,
+    orientation: 'up',
+    scale: 2,
+    overflow: true,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
+
+new simpleParallax(whale5, {
+    delay: 0,
+    orientation: 'up',
+    scale: 3,
+    overflow: true,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
+
+new simpleParallax(whale6, {
+    delay: 0,
+    orientation: 'up',
+    scale: 3,
     overflow: true,
     transition: 'cubic-bezier(0,0,0,1)'
 });
